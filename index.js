581091d4e53478a99153a20f9275494471d3a8ca
rupak07 <rpotdukhe@hawk.iit.edu>
2024-04-23 14:09:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ const app = express();
 
 // Sample course data
 const courses = [
-  {
-    id: 1,
-    name: 'Backend Development',
-    instructor: 'Raj Krishnan',
-    schedule: 'Thursday 6:25-9:05pm'
-  },
-  {
-    id: 2,
-    name: 'Business Innovation',
-    instructor: 'Mark Campbell',
-    schedule: 'Wednesday 6:00-8:30pm'
-  }
-];
+    {
+      id: 1,
+      name: 'Backend Development',
+      instructor: 'Raj Krishnan',
+      schedule: 'Thursday (6:25-9:05pm)'
+    },
+    {
+      id: 2,
+      name: 'Business Innovation',
+      instructor: 'Mark Campbell',
+      schedule: 'Wednesday (6:00-8:30pm)'
+    },
+    {
+      id: 3,
+      name: 'Full Stack Development',
+      instructor: 'Brian Bailey',
+      schedule: 'Tuesday (6:00-8:30pm)'
+    },
+    {
+      id: 4,
+      name: 'Human-Computer Interaction',
+      instructor: 'Calvin Nobles',
+      schedule: 'Monday (6:30-9:00pm)'
+    }
+  ];
 
 // Route to get all courses
 app.get('/courses', (req, res) => {
