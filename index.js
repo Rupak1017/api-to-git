0286c32e1db36f2907e01570f606ff9a42949dc5
rupak07 <rpotdukhe@hawk.iit.edu>
2024-04-25 02:48:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,56 @@ const app = express();
 
 // Sample course data
 const courses = [
-    {
-      id: 1,
-      name: 'Backend Development',
-      instructor: 'Raj Krishnan',
-      schedule: 'Thursday (6:25-9:05pm)'
-    },
-    {
-      id: 2,
-      name: 'Business Innovation',
-      instructor: 'Mark Campbell',
-      schedule: 'Wednesday (6:00-8:30pm)'
-    },
-    {
-      id: 3,
-      name: 'Full Stack Development',
-      instructor: 'Brian Bailey',
-      schedule: 'Tuesday (6:00-8:30pm)'
-    },
-    {
-      id: 4,
-      name: 'Human-Computer Interaction',
-      instructor: 'Calvin Nobles',
-      schedule: 'Monday (6:30-9:00pm)'
-    }
-  ];
+  {
+    id: 1,
+    name: 'Backend Development',
+    instructor: 'Raj Krishnan',
+    schedule: 'Thursday (6:25-9:05pm)'
+  },
+  {
+    id: 2,
+    name: 'Business Innovation',
+    instructor: 'Mark Campbell',
+    schedule: 'Wednesday (6:00-8:30pm)'
+  },
+  {
+    id: 3,
+    name: 'Full Stack Development',
+    instructor: 'Brian Bailey',
+    schedule: 'Tuesday (6:00-8:30pm)'
+  },
+  {
+    id: 4,
+    name: 'Human-Computer Interaction',
+    instructor: 'Calvin Nobles',
+    schedule: 'Monday (6:30-9:00pm)'
+  },
+  {
+    id: 5,
+    name: 'ITM Frameworks',
+    instructor: 'Rahul Patel',
+    schedule: 'Wednesday (6:25-9:00pm)'
+  },
+  {
+    id: 6,
+    name: 'Database Security',
+    instructor: 'Dawson Maurice',
+    schedule: 'Monday (3:30-5:30pm)'
+  },
+  {
+    id: 7,
+    name: 'Web App Development',
+    instructor: 'Anita Debarlaben',
+    schedule: 'Wednesday (1:50-3:05pm)'
+  },
+  {
+    id: 8,
+    name: 'Cyber Security Management',
+    instructor: 'Johnson Andrew',
+    schedule: 'Thursday (5:00-7:40pm)'
+  }
+];
+
 
 // Route to get all courses
 app.get('/courses', (req, res) => {
